ad19189564645ed188d04b4b8ea615c511d05836
carlos-m-a <carlos.ma.work@gmail.com>
2022-11-06 00:40:59 +0100

    refactor(django-basic-minimum): base template dir as a context argument
    Adds the direction to the base template as a context field

--- django-basic-minimum/django_basic_minimum/base/context_processors.py
+++ django-basic-minimum/django_basic_minimum/base/context_processors.py
@@ -3,5 +3,5 @@ from django.conf import settings
 def base_data(request):
     base_context = {}
     base_context['SITE_DOMAIN_NAME'] = settings.SITE_DOMAIN_NAME
-    base_context['BASE_TEMPLATE'] = settings.BASE_TEMPLATE
+    base_context['BASE_TEMPLATE_DIR'] = settings.BASE_TEMPLATE_DIR
     return base_context
