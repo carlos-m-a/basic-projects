908f64a218acace666f8b189e7e583075b8ecfbc
carlos-m-a <carlos.ma.work@gmail.com>
2022-11-06 00:38:32 +0100

    chore: delete project committed by mistake

--- django-basic-2/django_basic/app1/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-# Create your views here.
-
-@login_required
-def get_books(request):
-    return render(request=request, template_name="accounts/register.html")
--- django-basic-2/django_basic/base/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-
-
-def home(request):
-    return render (request=request, template_name="base/home.html")
