d29f5edab5a6228a9f4e96cf8d6efe6dee6c56c7
carlos-m-a <carlos.ma.work@gmail.com>
2022-11-05 15:12:30 +0100

    test(django-basic-minimum): Tests for views, urls and forms of accounts
    Test every accounts new logic

---
+++ django-basic-minimum/django_basic_minimum/accounts/tests_urls.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class RegistrationFormTestCase(TestCase):
+
+    def test_urls(self):
+        url = reverse('accounts:register')
+        self.assertEqual(url, '/accounts/register/')
+
+        url = reverse('accounts:login')
+        self.assertEqual(url, '/accounts/login/')
+
+        url = reverse('accounts:logout')
+        self.assertEqual(url, '/accounts/logout/')
+
+        url = reverse('accounts:password_change')
+        self.assertEqual(url, '/accounts/password_change/')
+
+        url = reverse('accounts:password_change_done')
+        self.assertEqual(url, '/accounts/password_change_done/')
+
+        url = reverse('accounts:password_reset')
+        self.assertEqual(url, '/accounts/password_reset/')
+
+        url = reverse('accounts:password_reset_confirm', kwargs={'uidb64':'A7A', 'token':'adf867sajdffJK'})
+        self.assertEqual(url, '/accounts/reset/A7A/adf867sajdffJK/')
+
+        url = reverse('accounts:password_reset_complete')
+        self.assertEqual(url, '/accounts/reset/')
+
+        url = reverse('accounts:update')
+        self.assertEqual(url, '/accounts/update/')
+
+        url = reverse('accounts:delete')
+        self.assertEqual(url, '/accounts/delete/')
+
+        url = reverse('accounts:profile')
+        self.assertEqual(url, '/accounts/profile/')
---
+++ django-basic-minimum/django_basic_minimum/accounts/tests_views.py
@@ -0,0 +1,191 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.http import urlencode
+from http import HTTPStatus
+from . import views
+from . import forms
+
+
+class NonAllowedRequestsTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = User()
+        user.username = 'user1'
+        user.email = 'user1@mail.com'
+        user.set_password('jajajeje11')
+        user.save()
+
+    def test_non_allowed_requests_for_authenticated_user(self):
+        self.client.login(username='user1', password='jajajeje11')
+        response = self.client.get(reverse('accounts:register'), follow=True)
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
+        response = self.client.get(reverse('accounts:login'), follow=True)
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
+        response = self.client.get(reverse('accounts:password_reset'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.get(reverse('accounts:password_reset_done'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.get(reverse('accounts:password_reset_confirm', kwargs={'uidb64':'A7A', 'token':'adf867sajdffJK'}), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.get(reverse('accounts:password_reset_complete'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.post(reverse('accounts:register'), follow=True)
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
+        response = self.client.post(reverse('accounts:login'), follow=True)
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL)
+        response = self.client.post(reverse('accounts:password_reset'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.post(reverse('accounts:password_reset_done'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.post(reverse('accounts:password_reset_confirm', kwargs={'uidb64':'A7A', 'token':'adf867sajdffJK'}), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        response = self.client.post(reverse('accounts:password_reset_complete'), follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_non_allowed_requests_for_non_authenticated_user(self):
+        response = self.client.get(reverse('accounts:password_change'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/password_change/' }))
+        response = self.client.get(reverse('accounts:password_change_done'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/password_change_done/' }))
+        response = self.client.get(reverse('accounts:profile'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/profile/' }))
+        response = self.client.get(reverse('accounts:update'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/update/' }))
+        response = self.client.get(reverse('accounts:delete'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/delete/' }))
+        response = self.client.post(reverse('accounts:password_change'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/password_change/' }))
+        response = self.client.post(reverse('accounts:password_change_done'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/password_change_done/' }))
+        response = self.client.post(reverse('accounts:profile'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/profile/' }))
+        response = self.client.post(reverse('accounts:update'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/update/' }))
+        response = self.client.post(reverse('accounts:delete'))
+        self.assertRedirects(response, reverse('accounts:login') + '?' + urlencode({'next': '/accounts/delete/' }))
+
+
+class ViewProfileTestCase(TestCase):
+    
+    def setUp(self):
+        user = User()
+        user.username = 'user1'
+        user.email = 'user1@mail.com'
+        user.set_password('jajajeje11')
+        user.save()
+        self.user = user
+        self.client.login(username='user1', password='jajajeje11')
+
+    def test_load(self):
+        response = self.client.get(reverse('accounts:profile'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.VIEW_PROFILE_TEMPLATE_FILE)
+        self.assertEqual
+
+
+class UpdateProfileTestCase(TestCase):
+    
+    def setUp(self):
+        user = User()
+        user.username = 'user1'
+        user.email = 'user1@mail.com'
+        user.set_password('jajajeje11')
+        user.save()
+        self.user = user
+        self.client.login(username='user1', password='jajajeje11')
+
+    def test_load(self):
+        response = self.client.get(reverse('accounts:update'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.UPDATE_PROFILE_TEMPLATE_FILE)
+        self.assertEqual(self.user.username, response.context['user'].username)
+
+    def test_invalid_data(self):
+        data={'email':'', 'current_password':'incorrectpassword'}
+        response = self.client.post(reverse('accounts:update'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.UPDATE_PROFILE_TEMPLATE_FILE)
+        self.assertEqual(self.user.username, response.context['user'].username)
+        self.assertEqual(type(response.context['form']), forms.UpdateUserForm)
+        self.assertEqual(len(response.context['form'].errors), 3)
+    
+    def test_valid_data(self):
+        data={'username':'user3', 'email':'user3@mail.com', 'first_name':'name3', 'current_password':'jajajeje11'}
+        response = self.client.post(reverse('accounts:update'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('accounts:profile'))
+        self.assertEqual(User.objects.filter(username='user3').count(), 1)
+        self.assertEqual(User.objects.filter(username='user1').count(), 0)
+        self.assertEqual(User.objects.get(username='user3').email, 'user3@mail.com')
+
+
+class DeleteProfileTestCase(TestCase):
+    
+    def setUp(self):
+        user = User()
+        user.username = 'user1'
+        user.email = 'user1@mail.com'
+        user.set_password('jajajeje11')
+        user.save()
+        self.user = user
+        self.client.login(username='user1', password='jajajeje11')
+
+    def test_load(self):
+        response = self.client.get(reverse('accounts:delete'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.DELETE_PROFILE_TEMPLATE_FILE)
+        self.assertEqual(self.user.username, response.context['user'].username)
+
+    def test_invalid_data(self):
+        data={'current_password':'incorrectpassword'}
+        response = self.client.post(reverse('accounts:delete'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.DELETE_PROFILE_TEMPLATE_FILE)
+        self.assertEqual(self.user.username, response.context['user'].username)
+        self.assertEqual(type(response.context['form']), forms.DeleteUserForm)
+        self.assertEqual(len(response.context['form'].errors), 1)
+
+
+    def test_valid_data(self):
+        data={'current_password':'jajajeje11'}
+        response = self.client.post(reverse('accounts:delete'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('accounts:login'))
+        self.assertEqual(User.objects.filter(username='user1').count(), 1)
+        self.assertEqual(User.objects.get(username='user1').is_active, False)
+    
+
+class RegisterTestCase(TestCase):
+    
+    def setUp(self):
+        user = User()
+        user.username = 'user1'
+        user.email = 'user1@mail.com'
+        user.set_password('jajajeje11')
+        user.save()
+        self.user = user
+
+    def test_load(self):
+        response = self.client.get(reverse('accounts:register'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.REGISTER_USER_TEMPLATE_FILE)
+
+    def test_invalid_data(self):
+        data={'email':'', 'password1':'jajajeje1', 'password2':'jajajeje2'}
+        response = self.client.post(reverse('accounts:register'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(type(response.context['form']), forms.NewUserForm)
+        self.assertTemplateUsed(response, views.REGISTER_USER_TEMPLATE_FILE)
+        self.assertEqual(len(response.context['form'].errors), 3)
+    
+    def test_valid_data(self):
+        data={'username':'user3', 'email':'user3@mail.com', 'first_name':'name3', 'password1':'jajajeje3', 'password2':'jajajeje3'}
+        response = self.client.post(reverse('accounts:register'), data=data)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, views.REGISTER_USER_DONE_TEMPLATE_FILE)
+        self.assertEqual(User.objects.filter(username='user3').count(), 1)
+        self.assertEqual(User.objects.all().count(), 2)
+        self.assertEqual(User.objects.get(username='user3').email, 'user3@mail.com')
