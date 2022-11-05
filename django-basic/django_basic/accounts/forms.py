84b54b491f3e88af24d5b1276c0287c07ff712f0
carlos-m-a <carlos.ma.work@gmail.com>
2022-11-05 15:09:13 +0100

    feat(django-basic): add class based views for every url
    Adds class based views for every url
    Move every message and data ckecking logic to forms
    New parameter in context_processors

--- django-basic/django_basic/accounts/forms.py
+++ django-basic/django_basic/accounts/forms.py
@@ -2,42 +2,73 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+ERROR_MESSAGE_INCORRECT_PASSWORD = _('Incorrect password')
+ERROR_MESSAGE_USERNAME_ALREADY_EXISTS = _('A user with that username already exists.')
+TEXT_PASSWORD_IS_REQUIRED = _('Password is required to allow this action')
 
 class NewUserForm(UserCreationForm):
-	email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        max_length=254,
+        label=_("Email"), 
+        widget=forms.EmailInput(attrs={"autocomplete": "email"}),
+    )
 
-	class Meta:
-		model = User
-		fields = ("username", "email", "password1", "password2")
-
-	def save(self, commit=True):
-		user = super(NewUserForm, self).save(commit=False)
-		user.email = self.cleaned_data['email']
-		if commit:
-			user.save()
-		return user
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
 
 
 class UpdateUserForm(forms.ModelForm):
-    username = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    current_password = forms.CharField(widget=forms.PasswordInput(), required=True,
-                                        label='Your current password', help_text='*Password is required to allow this action')
+    email = forms.EmailField(
+        required=True,
+        max_length=254,
+        label=_("Email"), 
+        widget=forms.EmailInput(attrs={"autocomplete": "email"}),
+    )
+    current_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}), 
+        required=True,
+        strip=False,
+        label=_('Current password'), 
+        help_text=TEXT_PASSWORD_IS_REQUIRED
+    )
 
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'current_password']
 
-    def clean(self):
+    def clean_username(self):
         username = self.cleaned_data['username']
-        current_password = self.cleaned_data['current_password']
-
         if User.objects.filter(username=username).exists() and username != self.instance.username:
-            self.add_error('username','This username already exists')
+            raise ValidationError(ERROR_MESSAGE_USERNAME_ALREADY_EXISTS, code='username_already_exists')
+        return username
+
+    def clean_current_password(self):
+        current_password = self.cleaned_data['current_password']
         if not check_password(current_password, self.instance.password):
-            self.add_error('current_password', 'Incorrect password')
+            raise ValidationError(ERROR_MESSAGE_INCORRECT_PASSWORD, code='password_invalid')
+        return current_password
 
 
-class DeleteUserForm(forms.Form):
-    password = forms.CharField(widget=forms.PasswordInput(), required=True)
+class DeleteUserForm(forms.ModelForm):
+    current_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"autocomplete": "current-password"}), 
+        required=True,
+        strip=False,
+        label=_('Password'), 
+        help_text=TEXT_PASSWORD_IS_REQUIRED
+    )
+
+    class Meta:
+        model = User
+        fields = ['current_password']
+
+    def clean_current_password(self):
+        current_password = self.cleaned_data['current_password']
+        if not check_password(current_password, self.instance.password):
+            raise ValidationError(ERROR_MESSAGE_INCORRECT_PASSWORD, code='password_invalid')
+        return current_password
