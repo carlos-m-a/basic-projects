fd05f12c587d2c2315ff7714f385eae9458b8dd0
carlos-m-a <carlos.ma.work@gmail.com>
2022-11-18 03:21:06 +0100

    chore(python-basic): forgotten test

--- python-basic/setup.py
+++ python-basic/setup.py
@@ -8,8 +8,10 @@ setuptools.setup(
    name='python_basic',
    version='1.0',
    description='Package description',
+   url='https://github.com/author/repository_name',
    author='Author Name',
    author_email='author@email.com',
+   license='BSD 2-clause',
    packages=['src'],
    install_requires=install_requires
 )
