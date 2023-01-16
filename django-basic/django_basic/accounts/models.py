9ab7a874095b5de2a031fedca8deedf076df1533
carlos-m-a <carlos.ma.work@gmail.com>
2023-01-16 14:12:07 +0100

    fix(django-basic): Solve some problems in admin with account

--- django-basic/django_basic/accounts/models.py
+++ django-basic/django_basic/accounts/models.py
@@ -1,22 +1,63 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.conf import settings
 
 class User(AbstractUser): 
     email = models.EmailField(
-        _("email"),
+        _("email address"),
         max_length=254,
         unique=True,
         error_messages={
-            "unique": _("A user with that email already exists."),
+            "unique": "A user with that email already exists.",
         },
+        blank=False,
+        null=False
     )
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar_image = models.ImageField(upload_to='images/', null=True, blank=True, max_length=254)
-    bio_text = models.TextField(blank=True, max_length=254)
-    date_of_birth = models.DateField(null=True, blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        null=False, 
+        related_name='profile'
+    )
+    avatar_image = models.ImageField(
+        upload_to='images/', 
+        null=True, 
+        blank=True, 
+        max_length=254
+    )
+    bio_text = models.TextField(
+        blank=True, 
+        max_length=254, 
+        null=True
+    )
+    date_of_birth = models.DateField(
+        null=True, 
+        blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+class Setting(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        null=False, 
+        related_name='setting'
+    )
+    activated_email_notifications = models.BooleanField(
+        null=False, 
+        default=False
+    )
+    language = models.SmallIntegerField(
+        null=False, 
+        default=1
+    )
 
     def __str__(self):
         return self.user.username
